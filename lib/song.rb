a583fbecc8f5ef00a2139085b5a392ecb3881e78
Diane Crame <dcrame@icloud.com>
2020-03-31 22:04:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
@@ -9,5 +8,27 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    #return song instance with song name set as name property
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    #finds song name and returns matching instance of the song with that name 
+    self.all.find{|song| song.name == song_name}
+  end
 
+  def self.destroy_all
+    @@all.clear
+  end
 end
