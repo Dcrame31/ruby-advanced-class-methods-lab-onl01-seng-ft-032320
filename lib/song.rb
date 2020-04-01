2eeab726a62c679dbc75e5631611afbb61168d07
Diane Crame <dcrame@icloud.com>
2020-04-01 00:41:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,6 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
@@ -9,5 +8,63 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    #return song instance with song name set as name property
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    #finds song name and returns matching instance of the song with that name 
+    self.all.detect {|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    self.all.include?(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+    
+  end
+  
+  def self.alphabetical
+    array = self.all.each{|song| song.name}
+      array.sort_by{|song| song.name}
+  end
 
+  def self.new_from_filename(file_name)
+    track_file = file_name.split(" - ")
+    track_file[1] = track_file[1].chomp(".mp3")
+    song = self.new
+    song.name = track_file[1]
+    song.artist_name = track_file[0]
+    song
+  end
+  
+  def self.create_from_filename(file_name)
+    track_file = file_name.split(" - ")
+    track_file[1] = track_file[1].chomp(".mp3")
+    song = self.new
+    song.name = track_file[1]
+    song.artist_name = track_file[0]
+    song.save
+    song
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
